e740f79d68308aed9d172f391020566d10de3d01
fyh <fyhtea@hotmail.com>
2017-11-30 11:11:48 +0800

    rbtree insertion

--- datastruct/tree/red_black_tree/rbtree.h
+++ datastruct/tree/red_black_tree/rbtree.h
@@ -26,7 +26,7 @@ public:
     Node(const Node<T> &x):key(x.key),color(x.color),parent(x.parent)
                            left(x.left),right(x.right) {} 
     //~Node(){ }
-}
+};
 
 
 template<typename T>
@@ -34,20 +34,144 @@ class RbTree{
     
     private:
     Node<T> *root;
-    const Node<T> *nil=nullptr; 
+    const Node<T> *nil; 
     
     public:
     RbTree();
     ~BsTree();
 
     private:
+    // (a, x, (b, y, c)) ===> ((a, x, b), y, c)
     void LeftRotate(Node<T> *x);
+    // ((a, x, b), y, c) ===> (a, x, (b, y, c))
     void RightRotate(Node<T> *x);
 
     public:
-    void TreeInsert(Node<T> *x);
+    void InsertFix(Node<T> *x);
+    void TreeInsert(T k);
+};
+
+template<typename T>
+inline void RbTree<T>::LeftRotate(Node<T> *x){
+    Node<T> *y = x->right;
+    x->right = y->left;
+    if (y->left != nil){
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if x->parent== nil{
+        root = y;
+    }else if(x == x->parent->left){
+        x->parent->left = y;
+    }else{
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
 }
 
+template<typename T>
+inline void RbTree<T>::RightRotate(Node<T> *x){
+    Node<T> *y = x->parent;
+    
+    y->left = x->right;
+    if (x->left != nil){
+        x->left->parent = y;
+    }
+    
+    x->parent = y->parent;
+    if(y->parent == nil){
+        root = x;
+    }else if(y == y->parent->left){
+        y->parent->left = x;
+    }else{
+        y->parent->right = x;
+    }
 
+    x->right = y;
+    y->parent = x;
+}
+
+template<typename T>
+inline void RbTree<T>::TreeInsert(T k){
+    
+    Node<T> *in_node = new Node<T>(k);
+    Node<T> *y = nil;
+    Node<T> *x = root;
+
+    while(x != nil){
+        y = x;
+        if (in_node->key < x->key){
+            x = x->left;
+        }else{
+            x = x->right;
+        }
+    }
+
+    in_node->parent = y;
+
+    if(y == nil){
+        root = x;
+    }else if(x->key < y->key){
+        y->left = x;
+    }else{
+        y->right = x;
+    }
+
+    x->left = nil;
+    x->right = nil;
+    x->color = Color::RED;
+
+    InsertFix(x);
+}
+
+template<typename T>
+inline void RbTree<T>::InsertFix(Node<T> *x){
+    Node<T> *y;
+    while(x->parent->color == Color::RED){
+        
+        if(x->parent == x->parent->parent->left){
+            y = x->parent->parent->right;
+            
+            if(y->color == Color::RED){                      //
+                x->parent->color = Color::BLACK;             //
+                y->color = Color::BLACK;                     //
+                x->parent->parent->color = Color::RED;       //
+                x = x->parent->parent;                       //  case1
+            }else{
+
+                if(x == x->parent->right){
+                    x = x->parent;                           //
+                    LeftRotate(x);                           //  case2->3
+                }
+
+                x->parent->color = Color::BLACK;             //
+                x->parent->parent->color = Color::RED;       //
+                RightRotate(x->parent->parent);              //  case3
+            }
+        
+        }else{
+            y = x->parent->parent->left;
+
+            if(y->color == Color::RED){
+                y->parent->color = Color::BLACK;
+                y->color = Color::BLACK;
+                x->parent->parent->color = Color::RED;
+                x = x->parent->parent;
+            }else{
+                
+                if(x == x->parent->left){
+                    x = x->parent;
+                    RightRotate(x);
+                }
+
+                x->parent->color = Color::BLACK;
+                x->parent->parent->color = Color::RED;
+                LeftRotate(x->parent->parent);
+            }
+        }
+    }
+    root->color = Color::BLACK;
+}
 
 #endif
