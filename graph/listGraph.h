bad7eaeb58da22d69d56e2bf0b1473dfeea719f2
fyh <fyhtea@hotmail.com>
2018-01-22 20:46:15 +0800

    graph

---
+++ graph/listGraph.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: listGraph.h
+	> Author:    fyhtea 
+	> Mail:      fyhtea@hotmail.com
+	> Created Time: 2018年01月22日 星期一 20时41分30秒
+ ************************************************************************/
+
+#ifndef _LISTGRAPH_H
+#define _LISTGRAPH_H
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+template<typename T_Vertex, typename T_Edge>
+class ListGraph{
+    friend ostream& operator<<(ostream &os, ListGraph<T_Vertex, T_Edge> &g);
+
+    private:
+}
+
+
+
+#endif
